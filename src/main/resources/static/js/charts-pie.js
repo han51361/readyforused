3132b0d06c05c1632d734c1548bc27c00005737e
han51361 <han51361@gmail.com>
2021-10-11 23:53:37 +0900

    차트 프론트 작업 수정

--- src/main/resources/static/js/charts-pie.js
+++ src/main/resources/static/js/charts-pie.js
@@ -6,16 +6,16 @@ const pieConfig = {
   data: {
     datasets: [
       {
-        data: [33, 33, 33],
+        data: [384, 229, 61, 50,33],
         /**
          * These colors come from Tailwind CSS palette
          * https://tailwindcss.com/docs/customizing-colors/#default-color-palette
          */
-        backgroundColor: ['#0694a2', '#1c64f2', '#7e3af2'],
+        backgroundColor: ['#0694a2', '#1c64f2', '#7e3af2','#FC9B00','#AFAFAF'],
         label: 'Dataset 1',
       },
     ],
-    labels: ['Shoes', 'Shirts', 'Bags'],
+    labels: ['번개장터', '네이버카페(중고나라 등)', '중고나라 어플','당근마켓','기타'],
   },
   options: {
     responsive: true,
